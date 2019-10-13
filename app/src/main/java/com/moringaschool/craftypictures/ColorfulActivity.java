84489f9b759aa19107200234e0280b944c9e54bc
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-13 17:40:58 +0200

    adding pictures in the colorful layout

--- app/src/main/java/com/moringaschool/craftypictures/ColorfulActivity.java
+++ app/src/main/java/com/moringaschool/craftypictures/ColorfulActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class ColorfulActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colorful);
