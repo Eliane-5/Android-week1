76995e54da0c5e781399463b7058bbb47e3eb3d1
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-13 14:38:17 +0200

    adding choice buttons

--- app/src/main/java/com/moringaschool/craftypictures/ChoiceActivity.java
+++ app/src/main/java/com/moringaschool/craftypictures/ChoiceActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -11,6 +13,9 @@ import butterknife.ButterKnife;
 
 public class ChoiceActivity extends AppCompatActivity {
     @BindView(R.id.userNameTextView) TextView mUserNameTextView;
+    @BindView(R.id.blackAndWhite) Button mblackAndWhiteButton;
+    @BindView(R.id.colorful) Button mColorfulButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,7 @@ public class ChoiceActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
         mUserNameTextView.setText("Hello "+  userName +", Please select a button to choose the type of pictures you would like to browse:");
+
+
     }
 }
